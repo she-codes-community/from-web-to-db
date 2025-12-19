07b629cb3e982417b61de83ee96ae1ecc00c374a
Yul As <locacow@gmail.com>
2025-12-19 15:18:21 +0200

    Lesson 9: User Authorization

---
+++ lesson9/auth.js
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken";
+import User from "./models/user.js";
+
+export async function auth(req, res, next) {
+    const header = req.headers.authorization;
+    if (!header) return res.status(401).json({ error: "Missing Authorization header" });
+
+    const [, token] = header.split(" ");
+    if (!token) return res.status(401).json({ error: "Missing token" });
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.userId);
+
+        if (!user) return res.status(401).json({ error: "User not found" });
+
+        req.userId = user._id;
+        req.userRole = user.role;
+
+        next();
+    } catch (err) {
+        return res.status(401).json({ error: "Invalid or expired token" });
+    }
+};
+
+export function requireRole(role) {
+    return function (req, res, next) {
+        if (!req.userRole) {
+            return res.status(401).json({ error: "User role missing" });
+        }
+
+        if (req.userRole !== role) {
+            return res.status(403).json({ error: "Forbidden – insufficient permissions" });
+        }
+
+        next();
+    };
+}
+
