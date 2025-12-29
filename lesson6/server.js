b291684c0bff88a2a3718b7cf7ddb11009cade9c
Yul As <locacow@gmail.com>
2025-12-29 14:56:06 +0200

    Lesson 2 & 6 small structural changes

--- lesson6/server.js
+++ lesson6/server.js
@@ -1,17 +1,19 @@
 import express from "express";
-import Book from "./models/book.js";
-import { connectDB } from "./db.js";
-
-connectDB();
-
 const app = express();
 
+/******************** For Mongoose ********************/
+import Book from "./mongomodels/book.js";
+import { connectDB } from "./mongodb.js";
+connectDB();
+
+/******************** Server ********************/
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
+
+/******************** Routes ********************/
 app.get("/api/books", async (req, res) => {
     const books = await Book.find();
     res.json(books);
 });
 
-
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
-});
