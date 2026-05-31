61256b7daaadaea1005d9eeb09e10973fc0f1d23
Roy Emek <roy.emek@gmail.com>
2026-05-31 14:52:59 +0300

    Remove Borrow model from lesson 12 (belongs in lesson 13)

---
+++ server/prisma/migrations/20260531200000_remove_borrow/migration.sql
@@ -0,0 +1,8 @@
+-- DropForeignKey
+ALTER TABLE "Borrow" DROP CONSTRAINT "Borrow_bookId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Borrow" DROP CONSTRAINT "Borrow_userId_fkey";
+
+-- DropTable
+DROP TABLE "Borrow";
