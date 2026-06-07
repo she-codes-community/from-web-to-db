ce5cfb23cebcf6531db2a1d313afc6d6566cf512
Roy Emek <roy.emek@gmail.com>
2026-06-07 10:35:48 +0300

    Revert lesson 05 to in-memory server — MongoDB introduced in lesson 06

--- server/mongomodels/book.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const bookSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    author: String,
-    year: Number
-});
-
-export default mongoose.model("Book", bookSchema);
